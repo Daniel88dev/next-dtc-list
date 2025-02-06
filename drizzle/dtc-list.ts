e6069230bb87e8618affcdc4e8faae6d51b8532d
Daniel88dev <64728456+Daniel88dev@users.noreply.github.com>
2025-02-06 16:49:10 +0100

    New drizzle query for searching detailed dtc list

--- drizzle/dtc-list.ts
+++ drizzle/dtc-list.ts
@@ -13,3 +13,18 @@ export async function getTopDtcBasic(dtc: string) {
     .where(like(dtcList.dtc, `%${dtc}%`))
     .limit(10);
 }
+
+export async function getDtcList(dtc: string) {
+  return db
+    .select({
+      id: dtcList.id,
+      dtc: dtcList.dtc,
+      description: dtcList.description,
+      type: dtcList.type,
+      system: dtcList.system,
+      item: dtcList.item,
+      detail: dtcList.detail,
+    })
+    .from(dtcList)
+    .where(like(dtcList.dtc, `%${dtc}%`));
+}
